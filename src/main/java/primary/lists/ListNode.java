2d0bae949ecd058bf88936f9f96ca64292860d09
jianxin.yuan <jianxin.yuan.zzz@gmail.com>
2020-03-16 14:12:31 +0800

    keep learning

---
+++ src/main/java/primary/lists/ListNode.java
@@ -0,0 +1,14 @@
+package primary.lists;
+
+/**
+ * @author yuan
+ * @date 2020/3/16 10:56
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
