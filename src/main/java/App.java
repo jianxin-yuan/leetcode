8d529666b7f78340154be255726e466d2596972f
jianxin.yuan <jianxin.yuan.zzz@gmail.com>
2020-04-07 15:23:17 +0800

    文字修改

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,18 @@
+import java.util.HashMap;
+
 /**
  * @author yuan
  * @date 2020/3/29 10:56 上午
  */
 public class App {
+
     public static void main(String[] args) {
 
-        int[] arr = {4,1,2,1,2};
-        System.out.println(onlyOne(arr));
+        HashMap map = new HashMap();
+        map.put("aa", "a");
+
+        Object o = new Object();
     }
 
-    public static int onlyOne(int[] arr) {
-        int res = 0;
-        for (int i : arr) {
-            res ^= i;
-        }
-        return res;
-    }
-    //public static int onlyOne2(int[] arr) {
-    //    int[] arr =
-    //}
+
 }
