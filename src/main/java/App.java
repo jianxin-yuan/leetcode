846f69808ab510a03a7e0481706c11ab7a85e7b7
yuan_jx <505673264@qq.com>
2020-04-19 18:41:12 +0800

    堆排序

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,18 +5,16 @@
 public class App {
     public static void main(String[] args) {
 
-        int[] arr = {4,1,2,1,2};
-        System.out.println(onlyOne(arr));
+
+
     }
 
-    public static int onlyOne(int[] arr) {
-        int res = 0;
-        for (int i : arr) {
-            res ^= i;
-        }
-        return res;
-    }
-    //public static int onlyOne2(int[] arr) {
-    //    int[] arr =
-    //}
+
+
+
+
+
+
+
+
 }
